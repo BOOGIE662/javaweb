e7e9dec8e910a28f69e6b5d34697db95547ba28f
BOOGIE <3208355342@qq.com>
2023-05-19 15:52:50 +0800

    Initial commit

---
+++ servlet07/src/servlet/WelcomeServlet.java
@@ -0,0 +1,24 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * description:
+ * author: BOOGIE
+ * data: 2023/3/3 11:25
+ */
+public class WelcomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.print("<h1>welcome to ...</h1>");
+    }
+}
